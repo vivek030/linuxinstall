39c472167ce8f6dea3dce880d8399472b7b56ffd
vivek magadum <66828419+vivek030@users.noreply.github.com>
2023-02-04 16:12:27 +0530

    Update linuxwg.sh

--- linuxwg.sh
+++ linuxwg.sh
@@ -1,19 +1,19 @@
 sudo -i
-sudo apt-get update
-sudo apt install curl
-sudo add-apt-repository ppa:wireguard/wireguard
-sudo apt-get update
-sudo apt-get install wireguard-dkms wireguard-tools
-sudo apt-get install openresolv
+apt-get update
+apt install curl
+add-apt-repository ppa:wireguard/wireguard
+apt-get update
+apt-get install wireguard-tools
+apt-get install openresolv
 dns = "10.6.6.1"
 read -p 'Enter Email ID ' email
 read -p 'Enter Friendly Name ' id
-hostname = hostname
+hostname = "hostname"
 name = "$id/$hostname"
 read -p 'Enter API Key ' api
 json = @"
 {"Email":"$email","Identifier":"$name","DNSStr":"$dns"}
 "@
 curl --silent  -f -k -X POST "https://newnew.korplink.com/api/v1/provisioning/peers" -H "accept: text/plain" -H "authorization: Basic $api" -H "Content-Type: application/json" -d $json -o "/etc/wireguard/wg0.conf"
-sudo systemctl start wg-quick@wg0
-sudo systemctl enable wg-quick@wg0
+systemctl start wg-quick@wg0
+systemctl enable wg-quick@wg0
